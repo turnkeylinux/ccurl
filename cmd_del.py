72f2cfef606d65e0ecfeaf8ffa125ef33e54c1a9
Alon Swartz <alon@turnkeylinux.org>
2013-04-10 16:57:43 +0300

    added note how to prune all files no longer in use

--- cmd_del.py
+++ cmd_del.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-# 
+#
 # Copyright (c) 2012 Alon Swartz <alon@turnkeylinux.org>
-# 
+#
 # This file is part of CCurl
-# 
+#
 # CCurl is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
 # Free Software Foundation; either version 3 of the License, or (at your
@@ -19,6 +19,13 @@ Arguments:
 Environment:
 
     CCURL_CACHE     Path to ccurl's cache (default: $HOME/.ccurl/global)
+
+Note:
+
+    To delete all files that aren't being used (ie. no hardlinks):
+    cd $HOME/.ccurl/global
+    find -type f -name "*.deb" -links 1 -delete
+
 """
 
 import sys
